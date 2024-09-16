be98f1d6bcae26c67fc8c9b713a9b77f5a77f145
fabianGerman <fabianontiveros_7@hotmail.com>
2024-09-16 02:21:07 -0300

    inicializacion

---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,23 @@
+<a href="/">
+    <!--
+    <svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
+        <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+    </svg>
+    -->
+
+    <svg class="w-24 h-24"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       " viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <!-- Círculo de fondo en celeste claro -->
+        <circle cx="50" cy="50" r="48" fill="#ADD8E6" />
+
+        <!-- Letra "S" en azul -->
+        <text x="28" y="60" font-size="40" fill="#6875F5" font-family="Arial, sans-serif" font-weight="bold">
+            S
+        </text>
+
+        <!-- Letra "J" en blanco -->
+        <text x="52" y="60" font-size="40" fill="#FFFFFF" font-family="Arial, sans-serif" font-weight="bold">
+            J
+        </text>
+    </svg>
+</a>
